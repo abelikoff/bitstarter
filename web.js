9e0d13153a2771bdc7fa515377d4d8a5248e51cc
Alexander L. Belikoff <abelikoff@gmail.com>
2013-07-24 00:28:54 -0400

    Serve images

--- web.js
+++ web.js
@@ -5,10 +5,20 @@ var fs = require('fs');
 app.get('/', function(request, response) {
     buf = fs.readFileSync("index.html");
     response.send(buf.toString());
-    //response.send('Hello World 2!');
+});
+
+app.get('/(images/[a-z][a-z0-9]*.png)', function(request, response) {
+    file = request.url.substring(1);
+    fs.exists(file, function(exists) {
+        if (exists) {
+            buf = fs.readFileSync(file);
+            response.writeHead(200, {'Content-Type': 'image/png' });
+            response.end(buf, 'binary');
+        }
+    });
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
